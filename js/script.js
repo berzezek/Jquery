7540368d23fb2ff41e220b9fe114e4dafeafb302
berzezek <wkduz@gmai.com>
2021-02-27 22:43:13 +0500

    v.1.0

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+
 $(".show").on("click", function () {
     $(".some").css('display', 'block');
+    $("p.some__new").css('display', 'block');
 });
 
 let counter = 0;
@@ -8,8 +10,10 @@ $(".insert").on("click", function () {
 
     if ($(".job").val() != "") {
         counter ++;
-        $("#spisok").after($('<p>').attr({'class':'some'}));
-        $("#spisok").next().text("Задание #"+ counter +"  " + $(".job").val());
+        $(".some").after($('<p>').attr({'class':'some'}));
+        $(".some").next().text("Задание #"+ counter + " " + $(".job").val());
+        $("p.some").prev().attr({'class':'some__new'});
+
         $(".elem").css('opacity', '1');
         $(".job").val("");
 
@@ -57,3 +61,10 @@ $(".btn").on("click", function () {
     $(".job").val("");
     $(".job").attr('placeholder', 'Задание');
 });
+
+$('h2').on("mouseover", function () {
+    $('img').css('display', 'block');
+})
+$('h2').on("mouseout", function () {
+    $('img').css('display', 'none');
+})
